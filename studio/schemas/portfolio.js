d6cc8a357a946c4c2957eae5a5f031f01dedac66
icld <ian@icldesign.com>
2021-10-13 12:58:52 -0700

    Add tech schema and reference

--- studio/schemas/portfolio.js
+++ studio/schemas/portfolio.js
@@ -25,8 +25,8 @@ export default {
     {
       name: 'technology',
       title: 'Technology',
-      type: 'array',
-      of: [{ type: 'string' }],
+      type: 'reference',
+      to: [{ type: 'tech' }],
     },
     {
       name: 'mainImage',
---
+++ studio/schemas/tech.js
@@ -0,0 +1,41 @@
+export default {
+  name: 'tech',
+  title: 'Tech',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'icon',
+      title: 'Icon',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+    },
+    {
+      name: 'url',
+      title: 'Url',
+      type: 'string',
+    },
+  ],
+};
