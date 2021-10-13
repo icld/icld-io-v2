c031d1adef4caa712a7ddac724e698f74700c0ed
icld <ian@icldesign.com>
2021-10-13 13:26:11 -0700

    Add comment schema

---
+++ studio/schemas/comment.js
@@ -0,0 +1,47 @@
+export default {
+  name: 'comment',
+  title: 'Comment',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'User Name',
+      type: 'string',
+    },
+    {
+      name: 'email',
+      title: 'Email',
+      type: 'string',
+    },
+    {
+      name: 'userImage',
+      title: 'User Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'comment',
+      title: 'Comment',
+      type: 'text',
+    },
+    {
+      name: 'childComments',
+      title: 'Child Comments',
+      type: 'array',
+      of: [{ type: 'comment' }],
+    },
+    {
+      name: 'approved',
+      title: 'Approved',
+      type: 'boolean',
+    },
+  ],
+  preview: {
+    select: {
+      title: 'name',
+      subtitle: 'comment',
+    },
+  },
+};
