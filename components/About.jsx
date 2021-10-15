612b7dae5c3d46acf04ac97da3ef6d4b685df834
icld <ian@icldesign.com>
2021-10-14 22:18:53 -0700

    Add chevron animation

--- components/About.jsx
+++ components/About.jsx
@@ -6,65 +6,44 @@ import { Link } from './Link';
 import { FiArrowLeft, FiArrowRight, FiArrowUpLeft } from 'react-icons/fi';
 export default function Example() {
   return (
-    <div className='relative mt-24 overflow-hidden bg-white'>
-      <div className='relative px-4 py-8 mx-auto lg:py-16 max-w-7xl sm:px-6 lg:px-8 '>
-        {/* <div className='absolute top-0 bottom-0 hidden w-screen lg:block bg-gray-50 left-3/4' /> */}
-        <div className='mx-auto text-base max-w-prose md:grid md:grid-cols-2 md:gap-8 md:max-w-none'></div>
-        <div className='grid font-dingo md:grid-cols-2 md:gap-8'>
-          <div className='relative row-start-2 md:row-start-1 md:col-start-2'>
-            <div className='relative h-full lg:aspect-none'>
-              <Image
-                className='object-cover rounded-lg shadow-lg '
-                src={icldImg}
-                alt='Ian staring off into nothing'
-                layout='responsive'
-                width={1184}
-                height={1376}
-              />
-            </div>
+    <div className='relative h-full px-4 py-8 mx-auto lg:py-16 max-w-7xl sm:px-6 lg:px-8'>
+      {/* <div className='absolute top-0 bottom-0 hidden w-screen lg:block bg-gray-50 left-3/4' /> */}
+      <div className='mx-auto text-base max-w-prose md:grid md:grid-cols-2 md:gap-8 md:max-w-none'></div>
+      <div className='grid font-dingo md:grid-cols-2 md:gap-8'>
+        <div className='relative row-start-2 md:row-start-1 md:col-start-2'>
+          <div className='relative h-full lg:aspect-none'>
+            <Image
+              className='object-cover rounded-lg shadow-lg '
+              src={icldImg}
+              alt='Ian staring off into nothing'
+              layout='responsive'
+              width={1184}
+              height={1376}
+            />
           </div>
-          <div className='font-normal tracking-wide max-w-prose lg:max-w-none'>
-            <h1 className='mb-2 text-3xl tracking-tight text-gray-800 font-another md:text-4xl lg:text-6xl '>
-              Ian Cameron Lyles
-            </h1>
-            <h3 className='mb-5 text-xl text-yellow-500 font-another'>
-              developer
-            </h3>
-            <p className='mb-3 text-sm text-gray-800 md:text-lg'>
-              Hello! I am a freelance developer living in Portland, Oregon. I
-              have a background in concert lighting design and production
-              management.
-            </p>
-            <p className='mb-3 text-sm text-gray-800 md:text-lg'>
-              I have a young daughter and an old dog. We love to go mountain
-              biking and run around in the woods.
-            </p>
-            <p className='mb-6 text-sm text-gray-800 md:text-lg '>
-              Coding and designing web applications is fun for me. I love
-              building something out of nothing. I enjoy the creative
-              relationships I get to develop with my clients, exploring dynamic
-              solutions to everyday problem solving, and discovering efficiences
-              that make their lives and businesses run smoothly.
-            </p>
-
-            {/* responsive link to contact */}
-            {/* <Link href='/contact' className=''>
-              {' '}
-              <div className='flex flex-col py-1 text-5xl text-center text-white transition-all delay-100 transform bg-gray-900 border-4 rounded-md shadow-sm font-normaltracking-widest group hover:font-extralight hover:border-transparent md:text-7xl hover:uppercase'>
-                <div className='group-hover:text-blue-400 '>What Would</div>
-                <div className='flex flex-row items-center justify-center'>
-                  {' '}
-                  <FiArrowRight className='hidden font-light group-hover:block group-hover:text-yellow-600 ' />
-                  <div>{`${' You'}`} </div>{' '}
-                  <FiArrowLeft className='hidden font-light group-hover:block group-hover:text-yellow-600' />
-                  <div className='group-hover:hidden'>Like To</div>
-                </div>
-                <div className='group-hover:tracking-widest group-hover:font-medium group-hover:text-blue-500 '>
-                  Build?
-                </div>
-              </div>
-            </Link> */}
-          </div>
+        </div>
+        <div className='font-normal tracking-wide max-w-prose lg:max-w-none'>
+          <h1 className='mb-2 text-3xl tracking-tight text-gray-800 font-another md:text-4xl lg:text-6xl '>
+            Ian Cameron Lyles
+          </h1>
+          <h3 className='mb-5 text-xl text-yellow-500 font-another'>
+            developer
+          </h3>
+          <p className='mb-3 text-sm text-gray-800 md:text-lg'>
+            Hello! I am a freelance developer living in Portland, Oregon. I have
+            a background in concert lighting design and production management.
+          </p>
+          <p className='mb-3 text-sm text-gray-800 md:text-lg'>
+            I have a young daughter and an old dog. We love to go mountain
+            biking and run around in the woods.
+          </p>
+          <p className='mb-6 text-sm text-gray-800 md:text-lg '>
+            Coding and designing web applications is fun for me. I love building
+            something out of nothing. I enjoy the creative relationships I get
+            to develop with my clients, exploring dynamic solutions to everyday
+            problem solving, and discovering efficiences that make their lives
+            and businesses run smoothly.
+          </p>
         </div>
       </div>
     </div>
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,37 +1,87 @@
+import { useRef, useState, useEffect } from 'react';
 import Layout from '../components/Layout';
 import Header from '../components/Header';
 import Image from 'next/image';
 import Footer from '../components/Footer';
+import About from '../components/About';
+import { useRouter } from 'next/router';
 import { motion } from 'framer-motion';
+import VisibilitySensor from 'react-visibility-sensor';
 
-import { BsCaretDown } from 'react-icons/bs';
+import { BsChevronDoubleDown } from 'react-icons/bs';
 import hero from '../public/hero_home.jpg';
 
 export default function Home() {
-  return (
-    <motion.section
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-      transition={{ duration: 0.5 }}
-      className='relative w-screen h-screen '
-    >
-      <div className='top-0 left-0 w-full mx-auto '>
-        <Image src={hero} alt='Street scenes' layout='fill' objectFit='cover' />
-      </div>
+  const router = useRouter();
+  const aboutRef = useRef(null);
+  const heroRef = useRef(null);
+  const [aboutIsVisible, setAboutIsVisible] = useState(false);
 
-      {/* Bouncing Down Arrow */}
-      <motion.div
-        variants={chevronVariants}
-        whileHover='hover'
-        whileTap='click'
-        initial={{ scale: 1 }}
-        animate='bounce'
-        className='absolute z-50 flex text-5xl text-white left-1/2 bottom-14'
+  // useEffect(() => {
+  //   // setAboutIsVisible(false);
+  //   return () => {
+  //     aboutIsVisible;
+  //   };
+  // }, []);
+
+  const scrollToRef = (ref) => {
+    window.scrollTo({
+      top: (0, ref.current.offsetTop),
+      behavior: 'smooth',
+    });
+  };
+
+  return (
+    <div className='flex flex-col' ref={heroRef}>
+      <motion.section
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.5 }}
+        className='relative w-screen h-screen '
       >
-        <BsCaretDown className='text-yellow-300' />
-      </motion.div>
-    </motion.section>
+        <div className='top-0 left-0 w-full mx-auto '>
+          <Image
+            src={hero}
+            alt='Street scenes'
+            layout='fill'
+            objectFit='cover'
+          />
+        </div>
+
+        {/* Bouncing Down Arrow */}
+        <motion.div
+          variants={chevronVariants}
+          whileHover='hover'
+          whileTap='click'
+          initial={{ scale: 1 }}
+          animate={aboutIsVisible ? 'pointingUp' : 'bounce'}
+          className={`fixed z-50 flex text-5xl text-white left-1/2 bottom-14  `}
+        >
+          <BsChevronDoubleDown
+            className={`text-yellow-300 ${aboutIsVisible && 'rotate-180'} `}
+            onClick={
+              aboutIsVisible
+                ? () => scrollToRef(heroRef)
+                : () => scrollToRef(aboutRef)
+            }
+          />
+        </motion.div>
+      </motion.section>
+
+      {/* About Section */}
+      <VisibilitySensor
+        partialVisibility={true}
+        minTopValue={250}
+        onChange={(isVisible) =>
+          isVisible ? setAboutIsVisible(true) : setAboutIsVisible(false)
+        }
+      >
+        <div className='relative' ref={aboutRef}>
+          <About />
+        </div>
+      </VisibilitySensor>
+    </div>
   );
 }
 
@@ -46,6 +96,10 @@ Home.getLayout = function getLayout(page) {
 };
 
 const chevronVariants = {
+  pointingUp: {
+    x: '800%',
+    transition: { x: { duration: 0.5 } },
+  },
   hover: {
     scale: 1.1,
     transition: { y: { stiffness: 1000, velocity: -100, duration: 0.5 } },
