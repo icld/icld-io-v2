d5c7631280bd4526b592c243ab7738b61015b8ec
icld <ian@icldesign.com>
2021-10-27 13:23:28 -0700

    Image opto

--- components/Auth/Avatar.jsx
+++ components/Auth/Avatar.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import Image from 'next/image';
 import { useUser } from '@auth0/nextjs-auth0';
 import { useRouter } from 'next/router';
 import { useModalStore } from '../../lib/zustand/store';
@@ -9,24 +9,26 @@ export default function Avatar() {
   const router = useRouter();
   return (
     <div
-      className={` flex-shrink-0  text-yellow-300 group font-another bg-gray-600 px-4 py-2 flex items-center justify-center rounded-md bg-opacity-50    shadow-2xl  hover:bg-opacity-60  transition  duration-300 hover:scale-110 `}
+      className={`  flex-shrink-0  text-yellow-300 group font-another bg-gray-600 px-4 py-2 flex items-center justify-center rounded-md bg-opacity-50    shadow-2xl  hover:bg-opacity-60  transition  duration-300 hover:scale-110 `}
     >
       {user ? (
         <button
-          className='flex items-center'
+          className='flex items-center overflow-visible transition duration-500 group-hover:scale-110 '
           onClick={() => setModalOpen(true)}
         >
           <div>
             {user && (
-              <img
-                className='inline-block w-8 h-8 transition duration-500 rounded-full group-hover:scale-110'
+              <Image
+                className='inline-block w-8 h-8 overflow-visible rounded-full '
                 src={user?.picture}
-                alt=''
+                width={32}
+                height={32}
+                alt='Logged in user avatar'
               />
             )}
           </div>
           <div className='ml-3'>
-            <p className='text-xl font-medium transition-transform duration-300 group-hover:text-yellow-200 group-hover:scale-125'>
+            <p className='overflow-visible text-xl font-medium transition-transform duration-300 group-hover:text-yellow-200 group-hover:scale-125'>
               {user?.nickname}
             </p>
             <p className='text-lg font-medium group-hover:text-gray-200'>
