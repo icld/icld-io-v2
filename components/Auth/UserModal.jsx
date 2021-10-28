85e511a44dee751a48bc726c77b5a11b8604112f
icld <ian@icldesign.com>
2021-10-27 18:38:26 -0700

    Add returnTo for logout

--- components/Auth/UserModal.jsx
+++ components/Auth/UserModal.jsx
@@ -99,7 +99,9 @@ export default function UserModal() {
                     messageOpen && 'hidden'
                   }`}
                   onClick={async () => {
-                    await router.push(`/api/auth/logout`);
+                    await router.push(
+                      `/api/auth/logout?returnTo=${router.asPath}`
+                    );
                     setModalOpen(false);
                   }}
                 >
